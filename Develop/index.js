9a9c60241b1b1f7047bb66103a8916d4486e25c2
Sharon <martinezsharonr@gmail.com>
2021-03-07 19:53:07 -0800

    added necessary questions in array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the authors name?",
+  "What is your GitHub username?",
+  "What is your email address?",
+  "What is your project's title?",
+  "Please write a brief description of your project:",
+  "What kind of license do you want for your project?",
+  "What command should be run to install dependencies?",
+  "What command should be run to run tests?",
+  "What should the user know about using the repo?",
+  "What does the user need to know about contributing to the repo?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
